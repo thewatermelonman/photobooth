27ef0da8219afcdbe421eac10a8d95ab5f8a159d
David Zwick <zwickdavid@yahoo.it>
2024-05-07 15:22:20 +0200

    Rewrite with overlay of logo

--- main.py
+++ main.py
@@ -1,37 +1,63 @@
 import cv2
-# from sh import lp
 import subprocess
+from PIL import Image, ImageWin
 
-def printPicture(img_counter):
-    # lp("pic_{}.jpeg".format(img_counter), "-y", magnify=2)
-    subprocess.run(["lp", "pic_{}.jpeg".format(img_counter), "-o", "scaling=75"])
-
-cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-cv2.namedWindow("photobooth")
+OVERLAY_FILE = "Konsens.jpg"
+TEST_JPG = "pics/test.jpg"
 
 img_counter = 0
+img2 = cv2.imread(OVERLAY_FILE)
+
+def overlayImage(img1):
+    print("[SAVING PICTURE...]")
+    assert img1 is not None, "file could not be read, check with os.path.exists()"
+    assert img2 is not None, "file could not be read, check with os.path.exists()"
+    rows,cols,channels = img2.shape
+    roi = img1[0:rows, 0:cols]
+    # Now create a mask of logo and create its inverse mask also
+    img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+    # Now black-out the area of logo in ROI
+    img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+    # Take only region of logo from logo image.
+    img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+    # Put logo in ROI and modify the main image
+    dst = cv2.add(img1_bg,img2_fg)
+    img1[0:rows, 0:cols ] = dst
+    
+    cv2.imshow("photo", img1)
+    img_name = "pics/test.jpg".format(img_counter)
+    cv2.imwrite(img_name, img1)
+    print("[SAVED PICTURE...]")
+
+def printImage():
+    print("[PRINTING...]")
+
+print("[STARTING...] (may take a bit)")
+cam = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 2480)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1748)
+
+print("[WEBCAM SETUP COMPLETE... ]")
+cv2.namedWindow("livepreview")
+cv2.namedWindow("photo")
 
 while True:
     ret, frame = cam.read()
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("photobooth", frame)
-
+    cv2.imshow("livepreview", frame)
     k = cv2.waitKey(1)
     if k%256 == 27: # ESC
-        print("Escape hit, closing...")
+        print("[EXIT PROGRAMM...]")
         break
     elif k%256 == 32: # SPACE
-        img_name = "pic_{}.jpeg".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        printPicture(img_counter)
-        img_counter += 1
+        print("[TAKING PICTURE...]")
+        overlayImage(frame)
 
 cam.release()
 cv2.destroyAllWindows()
+
 
